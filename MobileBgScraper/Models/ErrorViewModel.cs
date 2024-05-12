a9dbfe9f4a612b41ca3f6ca8bfa00406be2931b2
H3llBunny <bogo.dishlyanov@gmail.com>
2024-05-12 20:13:40 +0300

    We are going mongoDb baby!

--- MobileBgScraper/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MobileBgScraper.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
