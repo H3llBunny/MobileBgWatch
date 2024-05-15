e2dd15677137cac5639c85c6e74b35d8f0f367e1
H3llBunny <bogo.dishlyanov@gmail.com>
2024-05-15 23:46:30 +0300

    Added Login view, login and logout actions, updated the bootsrap in order for the datk theme to work

--- MobileBgScraper/MobileBgScraper/Controllers/UserController.cs
+++ MobileBgScraper/MobileBgScraper/Controllers/UserController.cs
@@ -1,21 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MobileBgScraper.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace MobileBgScraper.Controllers
 {
     public class UserController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public UserController(UserManager<ApplicationUser> userManager)
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager, SignInManager<ApplicationUser> signInManager)
         {
             this._userManager = userManager;
+            this._roleManager = roleManager;
+            this._signInManager = signInManager;
         }
 
         public IActionResult Register()
         {
-            return View();
+            return this.View();
         }
 
         [HttpPost]
@@ -32,7 +38,7 @@ namespace MobileBgScraper.Controllers
                 IdentityResult result = await this._userManager.CreateAsync(appUser, user.Password);
                 if (result.Succeeded)
                 {
-                    ViewBag.Message = "User created successfully!";
+                    return RedirectToAction("Login");
                 }
                 else
                 {
@@ -41,7 +47,46 @@ namespace MobileBgScraper.Controllers
                 }
             }
 
-            return View(user);
+            return this.View(user);
+        }
+
+        public IActionResult Login()
+        {
+            if (this._signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return this.View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(UserLogin user, string returnurl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser appUser = await this._userManager.FindByEmailAsync(user.Email);
+                if (appUser != null)
+                {
+                    Microsoft.AspNetCore.Identity.SignInResult result = await this._signInManager.PasswordSignInAsync(appUser, user.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return this.Redirect(returnurl ?? "/");
+                    }
+                }
+                ModelState.AddModelError("", "Login Failed: Invalid Email or Password");
+            }
+
+            return this.View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await this._signInManager.SignOutAsync();
+            return this.RedirectToAction("Index", "Home");
         }
     }
 }
