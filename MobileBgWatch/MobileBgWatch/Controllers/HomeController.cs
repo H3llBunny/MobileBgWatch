442c7c723941c14e17b85fcfa06321025a395439
H3llBunny <bogo.dishlyanov@gmail.com>
2024-07-19 22:24:06 +0300

    Comment removal

--- MobileBgWatch/MobileBgWatch/Controllers/HomeController.cs
+++ MobileBgWatch/MobileBgWatch/Controllers/HomeController.cs
@@ -100,11 +100,6 @@ namespace MobileBgWatch.Controllers
             }
 
             string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            //if (!await this._usersService.UserSearchUrlLimitAsync(userId))
-            //{
-            //    this.TempData["ErrorMessage"] = "You have reached the limit of 5 search URLs. I might add a monetization to expand it later in the development.";
-            //    return this.RedirectToAction(nameof(this.Index));
-            //}
 
             try
             {
